481cc40b6a30a51d10732508733e92cd99de91d0
kabedont <jjenniferjosephina@outlook.com>
2026-09-10 16:15:10 +0800

    feat: add session and passport middleware to index.js
    Signed-off-by: kabedont <jjenniferjosephina@outlook.com>

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const authRouter = require('./routes/authRouter')
+const authRouter = require('./routes/authRouter');
+const session = require("express-session");
+const passport = require("passport");
 
 app.use(cors());
 app.use (express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
 app.use("/api/auth", authRouter);
 
 app.get('/api/test', (req, res) => {
