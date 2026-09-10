632ab0f3e5bc8e6db8adbdf49266730d90afef57
kabedont <jjenniferjosephina@outlook.com>
2026-09-10 14:16:30 +0800

    feat: mount auth router in index.js
    Signed-off-by: kabedont <jjenniferjosephina@outlook.com>

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
 const app = express();
+const authRouter = require('./routes/authRouter')
 
 app.use(cors());
 app.use (express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRouter);
 
 app.get('/api/test', (req, res) => {
