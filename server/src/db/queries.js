758164cf431a4c51c4879125250a89c24f9d2b80
kabedont <jjenniferjosephina@outlook.com>
2026-09-12 16:03:04 +0800

    feat: add group query functions to queries.js
    Signed-off-by: kabedont <jjenniferjosephina@outlook.com>

--- server/src/db/queries.js
+++ server/src/db/queries.js
@@ -19,4 +19,40 @@ async function getUserById (id) {
     return rows;
 }
 
-module.exports = {signUpInformation, getUserByEmail, getUserById};
+async function insertGroup (name, created_by) {
+    await pool.query("INSERT INTO groups(name, created_by, created_at) VALUES ($1, $2, NOW())", [name, created_by]);
+}
+
+async function getAllGroups () {
+    const {rows} = await pool.query(
+        "SELECT * FROM groups"
+    );
+    return rows;
+}
+
+async function getGroupById (id) {
+    const {rows} = await pool.query(
+        "SELECT * FROM groups WHERE id = $1", [id]
+    );
+    return rows;
+}
+
+async function addMember (user_id, group_id) {
+    await pool.query("INSERT INTO members(user_id, group_id) VALUES ($1, $2)", [user_id, group_id]);
+}
+
+async function deleteGroup (id) {
+    await pool.query("DELETE FROM groups WHERE id = $1", [id]);
+}
+
+
+module.exports = {
+    signUpInformation, 
+    getUserByEmail, 
+    getUserById,
+    insertGroup,
+    getAllGroups,
+    getGroupById,
+    addMember,
+    deleteGroup
+};
