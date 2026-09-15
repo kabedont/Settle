3b7ab8852e4de6bd21f0d8157db147e0edd72825
kabedont <jjenniferjosephina@outlook.com>
2026-09-15 09:07:17 +0800

    feat: implement groups controller with db queries
    Signed-off-by: kabedont <jjenniferjosephina@outlook.com>

--- server/src/controllers/groupsController.js
+++ server/src/controllers/groupsController.js
@@ -1,23 +1,33 @@
 const db = require("../db/queries");
 
 async function createGroups(req, res) {
-    res.send("create groups");
+    const {name} = req.body;
+    const created_by = req.user.id;
+    await db.insertGroup(name, created_by);
+    res.json({message: "Group created successfully"});
 }
 
 async function getAllGroups(req, res) {
-    res.send("get all groups");
+    const groups = await db.getAllGroups();
+    res.json(groups);
 }
 
 async function getOneGroup(req, res) {
-    res.send("get one group");
+    const id = req.params.id;
+    const group = await db.getGroupById(id);
+    res.json(group[0]);
 }
 
 async function addMember(req, res) {
-    res.send("add member");
+    const {user_id, group_id} = req.body;
+    await db.addMember(user_id, group_id);
+    res.json({message: "Member added successfully"});
 }
 
 async function deleteGroup(req, res) {
-    res.send("delete group");
+    const id = req.params.id;
+    await db.deleteGroup(id);
+    res.json({message: "Group deleted successfully"});
 }
 
 module.exports = {
