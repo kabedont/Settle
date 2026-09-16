e539017f3a61d7231902feeec7ab40baa1497e7d
kabedont <jjenniferjosephina@outlook.com>
2026-09-16 14:54:17 +0800

    feat: implement expenses controller with db queries
    Signed-off-by: kabedont <jjenniferjosephina@outlook.com>

--- server/src/controllers/expensesController.js
+++ server/src/controllers/expensesController.js
@@ -1,19 +1,28 @@
 const db = require("../db/queries");
 
-async function createExpenses(req, res) {
-    res.send("create expenses");
+async function createExpenses(req, res) { 
+    const paid_by = req.user.id;
+    const group_id = req.params.id;
+    const {amount, currency, description} = req.body;
+    await db.insertExpenses(group_id, paid_by, amount, currency, description);
+    res.json({message: "expense created"});
 }
 
 async function getAllExpenses(req, res) {
-    res.send("get all expenses");
+    const expenses = await db.getAllExpenses();
+    res.json(expenses);
 }
 
 async function getOneExpense(req, res) {
-    res.send("get one expense");
+    const id = req.params.id;
+    const expense = await db.getExpenseById(id);
+    res.json(expense[0]);
 }
 
 async function deleteExpense(req, res) {
-    res.send("delete expense");
+    const id = req.params.id;
+    await db.deleteExpense(id);
+    res.json({message: "expense deleted"});
 }
 
 module.exports = {
