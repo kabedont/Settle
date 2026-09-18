0286881e6ac7acadc24d0081cecfb9f1348faa21
kabedont <jjenniferjosephina@outlook.com>
2026-09-18 08:29:15 +0800

    feat: add expense route to groups and verify split logic works
    Signed-off-by: kabedont <jjenniferjosephina@outlook.com>

--- server/src/routes/groupsRouter.js
+++ server/src/routes/groupsRouter.js
@@ -1,11 +1,13 @@
 const {Router} = require("express");
 const groupsRouter = Router();
 const groupsController = require("../controllers/groupsController");
+const expensesController = require("../controllers/expensesController");
 
 groupsRouter.post("/", groupsController.createGroups);
 groupsRouter.get("/", groupsController.getAllGroups);
 groupsRouter.get("/:id", groupsController.getOneGroup);
 groupsRouter.post("/:id/members", groupsController.addMember);
 groupsRouter.delete("/:id", groupsController.deleteGroup);
+groupsRouter.post("/:id/expenses", expensesController.createExpenses);
 
 module.exports = groupsRouter;
