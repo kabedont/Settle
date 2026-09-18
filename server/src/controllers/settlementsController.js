691babc2bc25cab4545f8a416e850ba666693cab
kabedont <jjenniferjosephina@outlook.com>
2026-09-18 09:47:58 +0800

    feat: implement settlements controller with db queries
    Signed-off-by: kabedont <jjenniferjosephina@outlook.com>

--- server/src/controllers/settlementsController.js
+++ server/src/controllers/settlementsController.js
@@ -1,9 +1,16 @@
+const db = require("../db/queries");
+
 async function createSettlement(req, res) {
-    res.json ({message: "settlement created"});
+    const group_id = req.params.id;
+    const from_user = req.user.id;
+    const {to_user, amount, currency} = req.body;
+    await db.insertSettlements(group_id, from_user, to_user, amount, currency);
+    res.json({message: "Settlement created successfully"});
 }
 
 async function getAllSettlements(req, res) {
-    res.json ({message: "get all settlements"});
+    const settlements = await db.getAllSettlements();
+    res.json(settlements);
 }
 
 module.exports = {
