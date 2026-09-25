6f42991b234f4987844881901d6039632775d967
kabedont <jjenniferjosephina@outlook.com>
2026-09-25 10:28:14 +0800

    fix: return json response from login instead of redirect

--- server/src/routes/authRouter.js
+++ server/src/routes/authRouter.js
@@ -6,10 +6,15 @@ const passport = require("passport");
 authRouter.get("/sign-up", authController.registerGet);
 authRouter.post("/sign-up", authController.registerPost);
 authRouter.get("/log-in", authController.loginGet);
-authRouter.post("/log-in", passport.authenticate("local", {
-    successRedirect: "/homepage",
-    failureRedirect: "/log-in",
-    failureMessage: true,
-}));
+authRouter.post("/log-in", (req, res, next) => {
+    passport.authenticate("local", (err, user, info) => {
+        if (err) return next(err);
+        if (!user) return res.status(401).json({ message: "invalid email or password" });
+        req.logIn(user, (err) => {
+            if (err) return next(err);
+            return res.status(200).json({ message: "logged in", user: { id: user.id, email: user.email, name: user.name } });
+        });
+    })(req, res, next);
+});
 
 module.exports = authRouter;
